a9eba6d9a983a1d896d2e32092b8603e85e63674
Avish Vijayaraghavan <avish.vijayaraghavan@gmail.com>
2021-05-29 17:12:03 +0000

    remove unnecessary folder and notebook cells

--- initial_tweaking/gnn.py
+++
@@ -1,9 +0,0 @@
-
-# compare deviation from D_init to final
-# tweak mit paper in small ways: standard gnn on R and P
-# then bayesian gnn after
-
-# identify green group model issues when applied with my framing
-
-# d_init tweaking; weight matrix tweaking
-
