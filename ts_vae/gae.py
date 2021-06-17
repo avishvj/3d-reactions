6b499c24d1091405de83450cb25c881b29384903
Avish <av1017@ic.ac.uk>
2021-06-17 09:31:04 +0100

    pt egnn layers and ae

--- ts_vae/gae.py
+++ ts_vae/gae.py
@@ -80,8 +80,6 @@ class GAE(nn.Module):
         return roc_auc_score(y, pred), average_precision_score(y, pred)
 
 
-
-
 ### other way
 
 class EdgeDecoder(nn.Module):
@@ -102,7 +100,202 @@ class NodeDecoder(nn.Module):
 
 # then could have CoordDecoder and GlobalDecoder, too
 
-from .layers import GCL
+from .layers import NELayer, NECLayer, GCL_PYG
+
+class EGNN(nn.Module):
+    def __init__(self, n_layers, feats_dim, pos_dim = 3, edge_attr_dim = 0, latent_dim = 2,
+                 embedding_nums = [], embedding_dims = [], edge_embedding_nums = [], edge_embedding_dims = [],
+                 update_coords = True, update_feats = True, norm_feats = True, norm_coords = False, norm_coords_scale_init = 1e-2,
+                 aggr = "add"):
+        
+        super().__init__()
+
+        self.n_layers = n_layers
+
+        # emb
+        self.embedding_nums = embedding_nums
+        self.embedding_dims = embedding_dims
+        self.emb_layers = nn.ModuleList()
+        self.edge_embedding_nums = edge_embedding_nums
+        self.edge_embedding_dims = edge_embedding_dims
+        self.edge_emb_layers = nn.ModuleList()
+
+        # instantiate node and edge embedding layers
+        for i in range(len(self.embedding_dims)):
+            self.emb_layers.append(nn.Embedding(num_embeddings = embedding_nums[i],
+                                                embedding_dim = embedding_dims[i]))
+            feats_dim += embedding_dims[i] - 1
+        for i in range(len(self.edge_embedding_dims)):
+            self.edge_emb_layers.append(nn.Embedding(num_embeddings = edge_embedding_nums[i],
+                                                     embedding_dim = edge_embedding_dims[i]))
+            edge_attr_dim += edge_embedding_dims[i] - 1
+        
+        # other params
+        self.mpnn_layers = nn.ModuleList()
+        self.feats_dim = feats_dim
+        self.pos_dim = pos_dim
+        self.edge_attr_dim = edge_attr_dim
+        self.latent_dim = latent_dim
+        self.norm_feats = norm_feats
+        self.norm_coords = norm_coords
+        self.norm_coords_scale_init = norm_coords_scale_init
+        self.update_feats = update_feats
+        self.update_coords = update_coords
+        
+        # instantiate layers
+        for i in range(n_layers):
+            egnn_layer = GCL_PYG(feats_dim = feats_dim, pos_dim = pos_dim, edge_attr_dim = edge_attr_dim,
+                                 latent_dim = latent_dim, norm_feats = norm_feats, norm_coords = norm_coords,
+                                 norm_coords_scale_init = norm_coords_scale_init, update_feats = update_feats, update_coords = update_coords)
+            self.mpnn_layers.append(egnn_layer)
+        
+    def forward(self, x, edge_index, batch, edge_attr):
+        # TODO: recalc edge features every recalc_edge
+
+        # nodes
+        x = embedding_token(x, self.embedding_dims, self.emb_layers)
+
+        # regulates whether to embedding edges each layer
+        edges_need_embedding = True
+        for i, layer in enumerate(self.mpnn_layers):
+
+            # edges
+            if edges_need_embedding:
+                edge_attr = embedding_token(edge_attr, self.edge_embedding_dims, self.edge_emb_layers)
+                edge_need_embedding = False
+
+            # pass layers
+            x = layer(x, edge_index, edge_attr, batch = batch, size = bsize)
+
+        return x
+
+    def __repr__(self):
+        return "EGNN Network of: {0} layers".format(len(self.mpnn_layers))
+
+def embedding_token(x, dims, layers):
+    stop_concat = -len(dims)
+    to_embedding = x[:, stop_concat: ].long()
+    for i, emb_layer in enumerate(layers):
+
+        # portion corresponding to 'to_embedding' gets dropped
+        x = torch.cat([x[:, : stop_concat], emb_layer(to_embedding[:, i])], dim = -1)
+        stop_concat = x.shape[-1]
+    return x
+
+
+class EGNN_Simple(nn.Module):
+
+    def __init__(self, in_nf, hidden, out, in_edge, device = 'cpu', act_fn = nn.ReLU(), 
+                 n_layers = 1, norm_coords = False):
+        
+        super(EGNN_Simple, self).__init__()
+        self.hidden = hidden
+        self.n_layers = n_layers
+        self.emb_in = nn.Linear(in_nf, hidden)
+        self.emb_out = nn.Linear(hidden, out)
+
+        for i in range(0, n_layers):
+            self.add_module("GCL_Simple_%d" % i, NECLayer(self.hidden, self.hidden, self.hidden, edges_in_dim = in_edge,
+                            act_fn = act_fn, norm_coords = norm_coords))
+        self.to(self.device)
+
+    def forward(self, h, x, edges, edge_attr):
+        h = self.emb_in(h)
+        for i in range(0, self.n_layers):
+            h, x, _ = self.modules["GCL_Simple_%d" % i](h, edges, x, edge_attr = edge_attr)
+        h = self.emb_out(h)
+        return h, x
+
+
+class EGNN_AE(nn.Module):
+    # simple one first
+
+    def __init__(self, h_nf, emb_nf = 4, num_node_fs = 11, n_layers = 1, act_fn = nn.ReLU(), device = 'cpu'):
+        super(EGNN_AE, self).__init__()
+
+        self.num_node_fs = num_node_fs
+        self.h_nf = h_nf
+        self.emb_nf = emb_nf
+        self.n_layers = n_layers
+        self.device = device
+        
+        # encoder
+        ne_layer = NELayer(in_nf = num_node_fs, out_nf = h_nf, h_nf = h_nf, edges_nf = 4, act_fn = act_fn)
+        self.add_module("NE", ne_layer)
+        self.fc_emb = nn.Linear(h_nf, emb_nf)
+
+        # decoder linear layer W and b
+        self.W = nn.Parameter(-0.1 * torch.ones(1)).to(device)
+        self.b = nn.Parameter(torch.ones(1)).to(device)
+        
+        self.to(device)
+    
+    def forward(self, node_feats, edge_index, edge_attr = None):        
+        # encode features then decode to adj
+        # could just encode features to coords then decode to adj
+
+        node_feats, edge_feats = self.encode(node_feats, edge_index, edge_attr)
+        return self.fc_emb(node_feats)
+
+        # adj_pred = self.decode(x) # TODO: sort out!!!
+        # return adj_pred
+
+    def encode(self, node_feats, edge_index, edge_attr):
+        # one (node + edge) layer
+        node_feats, edge_feats = self._modules["NE"](node_feats, edge_index, edge_attr)
+        return node_feats, edge_feats
+    
+    def decode(self, x):
+        return self.decode_from_x(x, W = self.W, b = self.b)
+
+    def decode_from_x(self, x, W = 10, b = -1, remove_diag = True):
+        # W, b: weights and biases in case no linear layer
+        x_a = x.unsqueeze(0)
+        x_b = torch.transpose(x_a, 0, 1) # (_, first dim to t(), second_dim to t())
+        X = (x_a - x_b) ** 2
+        
+        num_nodes = x.size(0)
+        X = X.view(num_nodes ** 2, -1) 
+        X = torch.sigmoid(W * torch.sum(X, dim = 1) + b) # linear layer
+        
+        adj_pred = X.view(num_nodes, num_nodes)
+        if remove_diag: # TODO: the pyg method adds self-loops, what do I want?
+            adj_pred = adj_pred * (1 - torch.eye(num_nodes).to(self.device))
+        
+        return adj_pred
+
+
+class EGNN_NEC(nn.Module):
+
+    def __init__(self, in_nf, out_nf, h_nf, in_ef, device = 'cpu', act_fn = nn.ReLU(), 
+                 n_layers = 1, norm_coords = False):
+        # nf: node features; ef: edge features
+        
+        super(EGNN_NEC, self).__init__()
+
+        self.h_nf = h_nf
+        self.device = device
+        self.n_layers = n_layers
+        self.emb_in = nn.Linear(in_nf, h_nf)
+        self.emb_out = nn.Linear(h_nf, out_nf)
+
+        # one NEC layer for now
+        self.add_module("NEC", NECLayer(in_nf = h_nf, out_nf = h_nf, hidden_nf = h_nf,
+                        edges_in_dim = in_ef, act_fn = act_fn, norm_coords = norm_coords))
+        
+        self.to(device)
+
+    def forward(self, node_feats, edge_index, edge_attr, coords):
+        # emb_in(in, hidden) -> NEC(hidden, hidden) -> emb_out(hidden, out)
+        node_feats = self.emb_in(node_feats)
+        # one NEC layer
+        node_feats, edge_feats, coords = self._modules["NEC"](node_feats, 
+                                                        edge_index, edge_attr, coords)
+        node_feats = self.emb_out(node_feats)
+        return node_feats, coords
+    
+        
+
 
 class BasicAE(nn.Module):
     # TODO: replace x -> z when encoding
@@ -127,6 +320,7 @@ class BasicAE(nn.Module):
 
     def encode(self, nodes, edges, edge_attr):
         # note: assumes all layers are GCL layers
+        # TODO: what is h???
         
         # initial layer
         h, _ = self._modules["GCL_0"](nodes, edges, edge_attr)
@@ -156,7 +350,9 @@ class BasicAE(nn.Module):
         else:
             X = torch.sigmoid(W * torch.sum(X, dim = 1) + b)
         
+        # .view() reshapes tensor into desired dim
         adj_pred = X.view(num_nodes, num_nodes)
+        
         if remove_diag:
             # eye returns 2D tensor with ones on diag and zeroes elsewhere
             # (1 - torch.eye(num_nodes)) gives [num_nodes, num_nodes] with all 1s except 0 on diag
--- ts_vae/layers.py
+++ ts_vae/layers.py
@@ -3,80 +3,237 @@ import torch
 import torch.nn as nn
 from torch_geometric.utils import (negative_sampling, remove_self_loops, add_self_loops)
 
-class GCL(nn.Module):
-    # basic graph convolutional layer
-    # TODO: make message passing
-
-    # no coord_model -> should maybe have this as an abstract base class to build from
-    # or an abstract class under both this basic model and the coord model
-
-    def __init__(self, input_nf, output_nf, hidden_nf, edges_in_nf = 0, act_fn = nn.ReLU(), bias = True):
-        super(GCL, self).__init__()
-        
-        input_edge_nf = input_nf * 2
-        self.edge_mlp = nn.Sequential(nn.Linear(input_edge_nf + edges_in_nf, hidden_nf, bias = bias),
-                                      act_fn)
-        
-        self.node_mlp = nn.Sequential(nn.Linear(hidden_nf + input_nf, hidden_nf, bias = bias),
-                                      act_fn)
-        
-        def edge_model(self, source, target, edge_attr):
-            edge_in = torch.cat([source, target], dim = 1)
-            if edge_attr is not None:
-                edge_in = torch.cat([edge_in, edge_attr], dim = 1)
-            out = self.edge_mlp(edge_in)
-            return out
-        
-        def node_model(self, h, edge_index, edge_attr):
-            row, col = edge_index
-            agg = unsorted_segment_sum(edge_attr, row, num_segments = h.size(0))
-            out = torch.cat([h, agg], dim = 1)
-            out = self.node_mlp(out)
-            return out
-
-def unsorted_segment_sum(edge_attr, segment_ids, num_segments):
-    result_shape = (num_segments, edge_attr.size(1))
-    result = edge_attr.new_full(result_shape, 0) # init empty result tensor
-    segment_ids = segment_ids.unsqueeze(-1).expand(-1, data.size(1))
-    result.scatter_add_(0, segment_ids, edge_attr) # adds all values from tesnor other int self at indices
-    return result
-
-
-# put this in layers
-
+# for pyg
 from torch_geometric.nn import MessagePassing
 from torch_geometric.utils import add_self_loops, degree
+from torch_geometric.nn.norm import LayerNorm
 
-class CoordGCLayer(MessagePassing):
+### class for coordinate normalisation
+
+class CoordsNorm(nn.Module):
+    # special module for coordinate normalisation
+    # uses strategy from se3 transformer normalisation
+    def __init__(self, eps = 1e-8, scale_init = 1.):
+        super().__init__()
+        self.eps = eps
+        scale = torch.zeros(1).fill_(scale_init)
+        self.scale = nn.Parameter(scale)
+    
+    def forward(self, coords):
+        norm = coords.norm(dim = -1, keepdim = True)
+        normed_coords = coords / norm.clamp(min = self.eps)
+        return normed_coords * self.scale
+
+### useful functions
+
+def unsorted_segment_sum(edge_attr, row, num_segments):
+    result_shape = (num_segments, edge_attr.size(1))
+    result = edge_attr.new_full(result_shape, 0) # init empty result tensor
+    row = row.unsqueeze(-1).expand(-1, edge_attr.size(1))
+    result.scatter_add_(0, row, edge_attr) # adds all values from tensor other int self at indices
+    return result
+
+### standard layer for nodes and edges, no coord input
+
+class NELayer(nn.Module):
+    # basic graph convolutional layer
+    # TODO: make message passing, maybe do abstract base class for this and coord or this as base for coords
+
+    def __init__(self, in_nf, out_nf, h_nf, edges_nf = 0, act_fn = nn.ReLU(), bias = True):
+        # in_nf: num of input node fs, out_nf: num of output node features, h_nf: num hidden node fs
+        # edges_nf: num of edge features
+
+        super(NELayer, self).__init__()
+        
+        # edge mlp
+        # in_nf * 2 for each node then add edge_nf
+        self.edge_mlp = nn.Sequential(nn.Linear(in_nf * 2 + edges_nf, h_nf, bias = bias),
+                                      act_fn)
+        # node mlp
+        self.node_mlp = nn.Sequential(nn.Linear(h_nf + in_nf, h_nf, bias = bias), 
+                                      act_fn,
+                                      nn.Linear(h_nf, out_nf, bias = bias))
+        
+    def edge_model(self, source, target, edge_attr):
+        edge_in = torch.cat([source, target, edge_attr], dim = 1)
+        return self.edge_mlp(edge_in)
+    
+    def node_model(self, node_feats, edge_index, edge_attr):
+        node_is, _ = edge_index
+        agg = unsorted_segment_sum(edge_attr, node_is, node_feats.size(0))
+        node_in = torch.cat([node_feats, agg], dim = 1)
+        return self.node_mlp(node_in)
+    
+    def forward(self, node_feats, edge_index, edge_attr):
+        node_is, node_js = edge_index
+        edge_feats = self.edge_model(node_feats[node_is], node_feats[node_js], edge_attr)
+        node_feats = self.node_model(node_feats, edge_index, edge_feats)
+        return node_feats, edge_feats
+
+
+### layer taking nodes, edges, coords as input
+
+class NECLayer(nn.Module):
+    # includes coords
+    # TODO: make MP!
+    
+    def __init__(self, in_nf, out_nf, hidden_nf, edges_in_dim = 0, act_fn = nn.ReLU(), norm_coords = False, coords_aggr = 'sum'):
+        
+        super(NECLayer, self).__init__()
+        
+        input_edge = in_nf * 2
+        edge_coords_nf = 1
+        self.norm_coords = norm_coords
+        self.coords_aggr = coords_aggr
+        self.epsilon = 1e-8
+
+        # edges
+        self.edge_mlp = nn.Sequential(nn.Linear(input_edge + edge_coords_nf + edges_in_dim, hidden_nf), act_fn)
+
+        # nodes
+        self.node_mlp = nn.Sequential(nn.Linear(hidden_nf + in_nf, hidden_nf), act_fn)
+
+        # coords
+        layer = nn.Linear(hidden_nf, 1, bias = False)
+        torch.nn.init.xavier_uniform_(layer.weight, gain = 0.001)
+        self.coords_mlp = nn.Sequential(nn.Linear(hidden_nf, hidden_nf), act_fn, layer)
+
+    def edge_model(self, source, target, edge_attr):
+        out = torch.cat([source, target, edge_attr], dim = 1)
+        return self.edge_mlp(out)
+    
+    def node_model(self, node_feats, edge_index, edge_attr):
+        row, col = edge_index
+        agg = unsorted_segment_sum(edge_attr, row, node_feats.size(0))
+        out = torch.cat([node_feats, agg], dim = 1)
+        return self.node_mlp(out)
+    
+    def coords_model(self, edge_index, edge_feats, coords, coords_diff):
+        row, col = edge_index
+        trans = coords_diff * self.coords_mlp(edge_feats)
+        if self.coords_aggr == 'sum':
+            aggr = unsorted_segment_sum(trans, row, coords.size(0))
+        else:
+            raise Exception('Only \'sum\' is valid as an aggregation function.')
+        coords += aggr
+        return coords
+
+    def forward(self, node_feats, edge_index, edge_attr, coords):
+        # h: node emb; e_i: coo graph connectivity; e_a: bond type; node_attr
+        # TODO: h vs node_attr?
+
+        row, col = edge_index
+        coords_diff = coords[row] - coords[col]
+        
+        if self.norm_coords:
+            # TODO: check if same as se3 norm method
+            radial = torch.sum(coords_diff ** 2, 1).unsqueeze(1)
+            norm = torch.sqrt(radial).detach() + self.epsilon
+            coords_diff /= norm
+        
+        edge_feats = self.edge_model(node_feats[row], node_feats[col], edge_attr)
+        coords = self.coords_model(edge_index, edge_feats, coords, coords_diff)
+        node_feats = self.node_model(node_feats, edge_index, edge_feats)
+
+        return node_feats, edge_feats, coords
+
+class GCL_PYG(MessagePassing):
     # MP layer
     # all logic takes place in forward() method: loops->transform->normalise->propagate->message
     # propagate() internally calls message(), aggregate(), update() functions
 
-    def __init__(self, in_channels, out_channels):
-        super(CoordGCLayer, self).__init__(aggr = 'add')
-        self.lin = torch.nn.Linear(in_channels, out_channels)
+    def __init__(self, feats_dim, pos_dim, edge_attr_dim = 0, latent_dim = 2,
+                 norm_feats = False, norm_coords = False, norm_coords_scale_init = 1e-2,
+                 update_feats = True, update_coords = True,
+                 aggr = "add", **kwargs):
+        # no soft_edge, fourier_features, dropout, clamp. still don't know what clamp does?
+        
+        assert aggr in {'add', 'sum', 'max', 'mean'}, "Aggregation method must be valid."
+        assert update_feats or update_coords, "You must update features, coordinates, or both."
+
+        super(GCL_PYG, self).__init__(**kwargs)
+        
+        # model parameters
+        self.feats_dim = feats_dim
+        self.pos_dim = pos_dim
+        self.latent_dim = latent_dim
+        self.norm_feats = norm_feats
+        self.norm_coords = norm_coords
+        self.update_feats = update_feats
+        self.update_coords = update_coords
+        self.edge_input_dim = edge_attr_dim + 1 + (feats_dim * 2) # num edge types + 2*num node feats + 1?
+
+        # edges
+        self.edge_mlp = nn.Sequential(nn.Linear(self.edge_input_dim, latent_dim), nn.ReLU())
+        self.edge_weight = None # self.edge_weight = nn.Sequential(nn.Linear(latent_dim, 1), nn.Sigmoid())
+
+        # nodes
+        self.node_norm = LayerNorm(feats_dim) if norm_feats else None
+        self.coords_norm = CoordsNorm(scale_init = norm_coords_scale_init) if norm_coords else nn.Identity()
+        self.node_mlp = nn.Sequential(nn.Linear(feats_dim + latent_dim, feats_dim), nn.ReLU()) if update_feats else None
+
+        # coordinates
+        self.coords_mlp = nn.Sequential(nn.Linear(latent_dim, 1)) if update_coords else None
+
+    def forward(self, x, edge_index, edge_attr, batch):
+
+        coords, feats = x[:, : self.pos_dim], x[:, self.pos_dim]
+
+        rel_coords = coords[edge_index[0]] - coords[edge_index[1]]
+        rel_dist = (rel_coords ** 2).sum(dim = -1, keepdim = True)
+
+        if edge_attr:
+            edge_attr_feats = torch.cat([edge_attr, rel_dist], dim = -1)
+        else:
+            edge_attr_feats = rel_dist
+        
+        hidden_out, coords_out = self.propagate(edge_index, x = feats, edge_attr = edge_attr_feats,
+                            coords = coords, rel_coords = rel_coords, batch = batch)
+        
+        return torch.cat([coords_out, hidden_out], dim = -1)
     
-    def forward(self, x, edge_index):
-        # x has shape [N, in_channels]; edge_index has shape [2, E]
-
-        # step 1: add self loops to adj matrix
-        edge_index, _ = add_self_loops(edge_index, num_nodes = x.size(0))
-
-        # step 2: transform matrix using networks
-        x = self.lin(x)
-
-        # step 3: compute normalisation
-        row, col = edge_index
-        deg = degree(col, x.size(0), dtype = x.dtype)
-        deg_inv_sqrt = deg.pow(-0.5)
-        norm = deg_inv_sqrt[row] * deg_inv_sqrt[col]
-
-        # step 4-5: start propagating messages
-        return self.propagate(edge_index, x = x, norm = norm)
+    def message(self, x_i, x_j, edge_attr):
+        m_ij = self.edge_mlp(torch.cat([x_i, x_j, edge_attr]))
+        return m_ij
     
-    def message(self, x_j, norm):
-        # x_j has shape [E, out_channels]
-        # x are node embeddings; norm are norm coefficients
+    def propagate(self, edge_index, size, **kwargs):
+        
+        size = self.__check_input__(edge_index, size)
+        coll_dict = self.__collect__(self.__user_args__, edge_index, size, kwargs)
+        
+        msg_kwargs = self.inspector.distribute('message', coll_dict)
+        aggr_kwargs = self.inspector.distribute('aggregate', coll_dict)
+        update_kwargs = self.inspector.distribute('update', coll_dict)
 
-        # step 4: normalise node features
-        return norm.view(-1, 1) * x_j
+        # get messages
+        m_ij = self.message(**msg_kwargs)
+
+        # update coordinates if specified
+        if self.update_coords:
+            coord_wij = self.coords_mlp(m_ij)
+            # TODO: clamp if arg set
+            # normalise if needed
+            kwargs["rel_coords"] = self.coords_norm(kwargs["rel_coords"])
+
+            mhat_i = self.aggregate(coord_wij * kwargs["rel_coords"], **aggr_kwargs)
+            coords_out = kwargs["coords"] + mhat_i
+        else:
+            coords_out = kwargs["coords"]
+        
+        # update features if specified
+        if self.update_feats:
+            # TODO: weight the edges if arg passed
+            m_i = self.aggregate(m_ij, **aggr_kwargs)
+
+            hidden_feats = self.node_norm(kwargs["x"], kwargs["batch"]) if self.node_norm else kwargs["x"]
+            hidden_out = self.node_mlp(torch.cat([hidden_feats, m_i], dim = -1))
+            hidden_out = kwargs["x"] + hidden_out
+        else:
+            hidden_out = kwargs["x"]
+        
+        # return tuple
+        return self.update((hidden_out, coords_out), **update_kwargs)
+    
+    def __repr__(self):
+        dict_print = {}
+        return "E(n)-GNN Layer for Graphs: " + str(self.__dict__)
