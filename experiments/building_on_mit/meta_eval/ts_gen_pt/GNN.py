e408a14384cce4b04a0c38ade9f0c2e7abc5ca7e
Avish <avish.vijayaraghavan@gmail.com>
2021-07-14 12:50:43 +0100

    done initial gnn

--- experiments/building_on_mit/meta_eval/ts_gen_pt/GNN.py
+++ experiments/building_on_mit/meta_eval/ts_gen_pt/GNN.py
@@ -7,23 +7,33 @@ from torch_scatter import scatter_sum
 
 class GNN(nn.Module):
 
-    def __init__(self, in_node_nf, in_edge_nf, num_epochs = 3, \
-        node_layers = 2, h_node_nf = 100, edge_layers = 2, h_edge_nf = 100):
+    def __init__(self, in_node_nf, in_edge_nf, num_iterations = 3, \
+        n_node_layers = 2, h_node_nf = 100, n_edge_layers = 2, h_edge_nf = 100):
         super(GNN, self).__init__()
+        # don't need masks because of PyTG batching
+        
+        self.num_iterations = num_iterations
 
-        # don't need masks because of PyTG
+        # init layers
+        self.node_mlp = MLP(in_node_nf, h_node_nf, n_node_layers)
+        self.edge_mlp = MLP(in_edge_nf, h_edge_nf, n_edge_layers)
+        
+        # edge layers 
+        out_edge_nf = h_node_nf
+        self.pf_layer = PairFeaturesLayer(in_node_nf, in_edge_nf, out_edge_nf)
+        self.de_mlp = MLP(out_edge_nf, h_edge_nf, n_edge_layers)
 
-        self.node_mlp = MLP(in_node_nf, h_node_nf, node_layers)
-        self.edge_mlp = MLP(in_edge_nf, h_edge_nf, edge_layers)
-
-        self.num_epochs = num_epochs
-        self.inner_layer = PairFeaturesLayer()
+        # node layers
+        self.dv_mlp1 = MLP(h_edge_nf, h_node_nf, n_node_layers)
+        self.dv_mlp2 = MLP(h_node_nf, h_node_nf, n_node_layers)
+        
     
-    def forward(self, node_feats, edge_index, edge_attr):
+    def forward(self, node_feats, edge_index, edge_attr, init = True):
         
         # init hidden states of nodes and edges
-        node_out = self.node_mlp(node_feats)
-        edge_out = self.edge_mlp(edge_attr)
+        if init:
+            node_out = self.node_mlp(node_feats)
+            edge_out = self.edge_mlp(edge_attr)
 
         # iteratively update edges (pair features, MLP, set final), nodes (MLP, reduce, MLP, set final)
         for _ in range(self.num_epochs):
@@ -33,21 +43,16 @@ class GNN(nn.Module):
         return node_out, edge_out
 
     def edge_model(self, node_feats, edge_index, edge_attr):
-        f = self.pair_features()
-        pass
+        f = self.pf_layer(node_feats, edge_index, edge_attr)
+        dE = self.de_mlp(f)
+        return edge_attr + dE
 
     def node_model(self, node_feats, edge_index, edge_attr):
-        pass
+        dV = self.dv_mlp1(edge_attr)
+        dV = torch.sum(dV, dim = 2) # TODO: figure out dim and change in mlp2
+        dV = self.dv_mlp2(dV)
+        return node_feats + dV
 
-    def pair_features(self, node_nf, edge_nf, out_nf, act = nn.ReLU()):
-        # lucky uses tf.expand_dims (like unsqueeze) here because of diff node and edge dims
-        # not sure if required here
-
-        f_ij = nn.Linear(edge_nf, out_nf, bias = True)
-        f_i = nn.Linear(node_nf, out_nf, bias = False) # first dim unsqueeze?
-        f_j = nn.Linear(node_nf, out_nf, bias = False) # second dim unsqueeze?
-
-        return act(f_ij + f_i + f_j)
 
 class PairFeaturesLayer(nn.Module):
 
@@ -55,11 +60,13 @@ class PairFeaturesLayer(nn.Module):
         super(PairFeaturesLayer, self).__init__()
         
         self.edge_ij_layer = nn.Linear(edge_nf, out_nf, bias = True)
-        self.node_i_layer = nn.Linear(node_nf, out_nf, bias = False)
-        self.node_j_layer = nn.Linear(node_nf, out_nf, bias = False) 
+        self.node_i_layer = nn.Linear(node_nf, out_nf, bias = False) # first dim unsqueeze?
+        self.node_j_layer = nn.Linear(node_nf, out_nf, bias = False) # second dim unsqueeze?
         self.act = act
 
     def forward(self, node_feats, edge_index, edge_attr):
+        # lucky uses tf.expand_dims (like unsqueeze) here because of diff node and edge dims
+        # not sure if required here
 
         node_is, node_js = edge_index
         node_is_fs, node_js_fs = node_feats[node_is], node_feats[node_js]
@@ -70,9 +77,10 @@ class PairFeaturesLayer(nn.Module):
 
         return self.act(f_ij + f_i + f_j)
 
+
 class MLP(nn.Module):
     def __init__(self, in_nf, out_nf, n_layers, act = nn.ReLU()):
-        # add norm layers?
+        # TODO: add norm layers?
         super(MLP, self).__init__()
         h_nf = in_nf
 
